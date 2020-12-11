0aa8825d9e565a153c759adf8f676d073784ce50
Wade Leftwich <wleftwich@gmail.com>
2020-12-11 16:08:07 -0500

    Create 11_seating_system.py

---
+++ 11_seating_system.py
@@ -0,0 +1,89 @@
+with open(datafile) as fh:
+    empty_chart_text = fh.read()
+    
+
+def text2chart(txt):
+    D = {}
+    for i, row in enumerate(txt.split()):
+        for  j, val in enumerate(row):
+            D[complex(i, j)] = val
+    return D
+
+# Part 1
+
+def changeseat(chart, seatkey):
+    seat = chart[seatkey]
+    if seat == '.':
+        return seat
+    occupied = 0
+    for drxn in [-1, -1+1j, 1j, 1+1j, 1, 1-1j, -1j, -1-1j]:
+        if chart.get(seatkey + drxn) == '#':
+            occupied += 1
+    if seat == 'L' and occupied == 0:
+        return '#'
+    if seat == '#' and occupied > 3:
+        return 'L'
+    return seat
+
+
+def nextchart(chart):
+    return {key: changeseat(chart, key) for key in chart.keys()}
+
+
+def run_till_stabilized(chart):
+    nc = nextchart(chart)
+    while nc != chart:
+        chart, nc = nc, nextchart(nc)
+    return nc
+
+
+def count_occupied(chart):
+    return sum(v == '#' for v in chart.values())
+
+
+empty_chart = text2chart(empty_chart_text)
+stabilized = run_till_stabilized(empty_chart)
+part_1 = count_occupied(stabilized)
+
+# Part 2
+
+
+def changeseat2(chart, seatkey):
+    seat = chart[seatkey]
+    if seat == '.':
+        return seat
+    occupied = 0
+    for drxn in [-1, -1+1j, 1j, 1+1j, 1, 1-1j, -1j, -1-1j]:
+        scale = 0
+        while True:
+            scale += 1
+            nkey = (drxn * scale) + seatkey
+            if nkey not in chart:
+                break
+            neighbor = chart[nkey]
+            if neighbor == '.':
+                pass
+            else:
+                if neighbor == '#':
+                    occupied += 1
+                break
+    if seat == 'L' and occupied == 0:
+        return '#'
+    if seat == '#' and occupied > 4:
+        return 'L'
+    return seat
+
+
+def nextchart2(chart):
+    return {key: changeseat2(chart, key) for key in chart.keys()}
+
+
+def run_till_stabilized2(chart):
+    nc = nextchart2(chart)
+    while nc != chart:
+        chart, nc = nc, nextchart2(nc)
+    return nc
+
+
+stabilized2 = run_till_stabilized2(empty_chart)
+part_2 = count_occupied(stabilized2)
