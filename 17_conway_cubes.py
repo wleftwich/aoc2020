3c6b6c0f7bb1c1b664e4c20612239927a1bc350f
Wade Leftwich <wleftwich@gmail.com>
2020-12-17 22:16:01 -0500

    Update 17_conway_cubes.py

--- 17_conway_cubes.py
+++ 17_conway_cubes.py
@@ -1,14 +1,13 @@
 from itertools import product
 
 def read_data(data, dim):
-    """Origin at sw corner"""
-    D = {}
+    S = set()
     for (i, row) in enumerate(reversed(data.split())):
         for (j, val) in enumerate(row):
             if val == '#':
                 coords = [i, j] + [0] * (dim - 2)
-                D[tuple(coords)] = 1
-    return D
+                S.add(tuple(coords))
+    return S
 
 def make_deltas(dim):
     origin = tuple([0] * dim)
@@ -20,8 +19,8 @@ def add_tuples(a, b):
 def apply_deltas(p, deltas):
     return (add_tuples(p, d) for d in deltas)
 
-def next_coords(D):
-    ks = list(D)
+def iter_coords(S):
+    ks = list(S)
     dim = len(ks[0])
     ranges = []
     for d in range(dim):
@@ -32,17 +31,15 @@ def next_coords(D):
 
 def cycle(cube):
     deltas = make_deltas(len(next(iter(cube))))
-    D = {}
-    for k in next_coords(cube):
-        v = cube.get(k)
+    S = set()
+    for k in iter_coords(cube):
         active_nabe_count = 0
         for nk in apply_deltas(k, deltas):
-            if cube.get(nk):
+            if nk in cube:
                 active_nabe_count += 1
-        if (v and active_nabe_count in [2, 3]) or (not v and active_nabe_count == 3):
-            D[k] = 1
-    return D
-
+        if active_nabe_count == 3 or (k in cube and active_nabe_count == 2):
+            S.add(k)
+    return S
 
 testdata = """\
 .#.
@@ -53,29 +50,29 @@ testdata = """\
 cube3 = read_data(testdata, 3)
 for _ in range(6):
     cube3 = cycle(cube3)
-part_1 = sum(v == '#' for v in cube3.values())
+part_1 = len(cube3)
 # 112
-# took 111 msec
+# took 82.1 msec
 
 cube4 = read_data(testdata, 4)
 for _ in range(6):
     cube4 = cycle(cube4)
-part_2 = sum(v == '#' for v in cube4.values())
+part_2 = len(cube4)
 # 848
-# took 3.76 sec
+# took 3.44 sec
 
 cube5 = read_data(testdata, 5)
 for _ in range(6):
     cube5 = cycle(cube5)
-bonus = sum(v == '#' for v in cube5.values())
+bonus = len(cube5)
 # 5760
-# took 2 min 21 sec
+# took 2 min 10 sec
 
 cube2 = read_data(testdata, 2)
 for _ in range(6):
     cube2 = cycle(cube2)
-flatland = sum(v == '#' for v in cube2.values())
+flatland = len(cube2)
 # 5
-# took 2.79 msec
+# took 1.05 msec
 
 
