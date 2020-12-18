666785fdda8addbc973ccbe71226f4e83ba3bafb
Wade Leftwich <wleftwich@gmail.com>
2020-12-18 15:39:36 -0500

    Update 17_conway_cubes.py

--- 17_conway_cubes.py
+++ 17_conway_cubes.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from itertools import product
 
 def read_data(data, dim):
@@ -16,19 +17,16 @@ def add_tuples(a, b):
 
 def cycle(actives):
     result = set()
-    inactives = set()
+    inactives = defaultdict(int)
     deltas = list(product((-1, 0, 1), repeat=len(next(iter(actives)))))
     for cube in actives:
         neighborhood = set(add_tuples(cube, d) for d in deltas)
         if len(neighborhood.intersection(actives)) in [3, 4]:
             result.add(cube)
-        inactives.update(neighborhood.difference(actives))
-    for cube in inactives:
-        neighborhood = set(add_tuples(cube, d) for d in deltas)
-        if len(neighborhood.intersection(actives)) == 3:
-            result.add(cube)
+        for inactive in neighborhood.difference(actives):
+            inactives[inactive] += 1
+    result.update(k for k, v in inactives.items() if v == 3)
     return result
-
 
 testdata = """\
 .#.
@@ -41,28 +39,28 @@ for _ in range(6):
     cube3 = cycle(cube3)
 part_1 = len(cube3)
 # 112
-# took 55.8 msec
+# took 15 msec
 
 cube4 = read_data(testdata, 4)
 for _ in range(6):
     cube4 = cycle(cube4)
 part_2 = len(cube4)
 # 848
-# took 1.56 sec
+# took 163 msec
 
 cube5 = read_data(testdata, 5)
 for _ in range(6):
     cube5 = cycle(cube5)
 d5 = len(cube5)
 # 5760
-# took 1 min
+# took 4.9 sec
 
 cube2 = read_data(testdata, 2)
 for _ in range(6):
     cube2 = cycle(cube2)
 flatland = len(cube2)
 # 5
-# took 967 microsec
+# took 850 microsec
 
 import time
 t0 = time.time()
@@ -71,9 +69,10 @@ for _ in range(6):
     cube6 = cycle(cube6)
     print(len(cube6), time.time() - t0)
 d6 = len(cube6)
-# 245 1.339268684387207
-# 464 10.454355955123901
-# 15744 74.04311347007751
-# (quit)
-
+# 245 0.007897377014160156
+# 464 0.2726321220397949
+# 15744 0.8170702457427979
+# 2240 19.55064821243286
+# 103552 22.021578788757324
+# 35936 144.75871062278748
 
